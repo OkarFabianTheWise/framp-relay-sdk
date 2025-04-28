25b2355e9a415a94fb50b7a48068c71f0b874281
OkarFabianTheWise <108690496+OkarFabianTheWise@users.noreply.github.com>
2025-04-28 16:23:03 +0100

    Delete dist directory

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { FrampRelayer } from './modules/relayer';
-export { sendToken } from './services/gift-token';
-export { Airtime, confirmAirtimeTransaction } from './services/gift-airtime';
-export type { GiftParams, AirtimeParams, TransactionResult, RelayerConfig } from './types';
